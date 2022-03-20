ffadf6789093c54c2242210681e1b83f2e746413
Lilizhi <1307713043@qq.com>
2022-03-20 20:15:04 +0800

    week3

--- layoutdemo/src/main/java/com/he/layoutdemo/MainActivity.java
+++ layoutdemo/src/main/java/com/he/layoutdemo/MainActivity.java
@@ -1,21 +1,17 @@
-package com.he.layouttest;
+package com.he.layoutdemo;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText EditText1;
     private EditText EditText2;
-    private EditText EditText3;
     private Button button;
-    private LinearLayout myLayout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +20,26 @@ public class MainActivity extends AppCompatActivity {
 
         EditText1 = (EditText) findViewById(R.id.editText1);
         EditText2 = (EditText) findViewById(R.id.editText2);
-        EditText3 = (EditText) findViewById(R.id.editText3);
         button = (Button) findViewById(R.id.button);
-        myLayout = (LinearLayout) findViewById(R.id.linearlayout);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText3.setText("");
-                button.setBackgroundColor(Color.RED);
+                String username="201958501313";
+                String password="123456";
+                if(username.equals(EditText1.getText().toString())==true&&password.equals(EditText2.getText().toString())==true) {
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("num", 1);
+
+                    Intent intent = new Intent(MainActivity.this, welcome.class);
+                    intent.putExtras(bundle);
+
+                    startActivityForResult(intent, 1);
+                }
+                else{
+                       Toast.makeText(MainActivity.this, "学号或密码有误请重新输入！", Toast.LENGTH_LONG).show();
+                }
             }
         });
-
     }
 }
